a0079991eeb3e5db044d392be7b2cdb42f5a6739
Ozan Er <58638988+ozn77@users.noreply.github.com>
2021-07-13 21:40:50 +0300

    sayfalar eklendi

--- lib/app/nutrition.dart
+++ lib/app/nutrition.dart
@@ -4,8 +4,9 @@ class NutritionPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(" page"),),
+      appBar: AppBar(title: Text("Nutrition"),),
       body: Center(child: Text("Nutrition Page"),),
     );
   }
 }
+
