93ee79a67e3b1953f8e9a4e9ac8e016a1bd83a7b
Ozan Er <58638988+ozn77@users.noreply.github.com>
2021-07-14 13:50:15 +0300

    Sports sayfası yenilik eklendi.

--- lib/app/sports.dart
+++ lib/app/sports.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:lifecoach_app/app/reusable_card.dart';
 
+import 'package:lifecoach_app/app/sports_page_extra/bodybuilding_page.dart';
+import 'package:lifecoach_app/app/sports_page_extra/bottom_button.dart';
+
 class SportsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -8,70 +11,80 @@ class SportsPage extends StatelessWidget {
       appBar: AppBar(title: Text("Sports"),),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Expanded(
-            child: ReusableCard(
-              colour: Colors.deepPurpleAccent,
-              cardChild: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    'Body Building',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.baseline,
-                    textBaseline: TextBaseline.alphabetic,
-                  )
-                ],
-              ),
-            ),
-          ),
+       children: <Widget>[
+         Expanded(
+           child: ReusableCard(
 
-          Expanded(
-            child: ReusableCard(
-              colour: Colors.deepPurpleAccent,
-              cardChild: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    'Running',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.baseline,
-                    textBaseline: TextBaseline.alphabetic,
-                  )
-                ],
-              ),
-            ),
-          ),
-          Expanded(
-            child: ReusableCard(
-              colour: Colors.white,
-              cardChild: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    'Running',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.baseline,
-                    textBaseline: TextBaseline.alphabetic,
-                  )
-                ],
-              ),
-            ),
-          ),
+             onPress: () {
+               Navigator.push(context,
+                   MaterialPageRoute(builder: (context)=> BodyBuildingPage())
+               );
+
+             },
+             colour: Colors.deepPurpleAccent,
+             images: Image.asset('images/bodybuilding.pose.jpeg'),
+
+             cardChild: Column(
+               mainAxisAlignment: MainAxisAlignment.center,
+               children: <Widget>[
+                 Text(
+                   'BODYBUILDING',
+                   style: TextStyle(color: Colors.white),
+                 ),
+                 Row(
+                   mainAxisAlignment: MainAxisAlignment.center,
+                   crossAxisAlignment: CrossAxisAlignment.baseline,
+                   textBaseline: TextBaseline.alphabetic,
+                   children: <Widget>[
+
+
+                   ],
+                 ),
+
+               ],
+             ),
+           ),
+         ),
+         Expanded(
+           child: ReusableCard(
+             onPress: () {
+               Navigator.push(context,
+                   MaterialPageRoute(builder: (context)=> BodyBuildingPage())
+               );
+
+             },
+             colour: Colors.deepPurpleAccent,
+             cardChild: Column(
+               mainAxisAlignment: MainAxisAlignment.center,
+               children: <Widget>[
+                 Text(
+                   'RUNNING',
+                   style: TextStyle(color: Colors.white),
+                 ),
+                 Row(
+                   mainAxisAlignment: MainAxisAlignment.center,
+                   crossAxisAlignment: CrossAxisAlignment.baseline,
+                   textBaseline: TextBaseline.alphabetic,
+                   children: <Widget>[
+
+
+                   ],
+                 ),
+
+               ],
+             ),
+           ),
+         ),
 
         ],
+
+
+
 
       ),
 
     );
   }
 }
+
+
---
+++ lib/app/sports_page_extra/bodybuilding_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BodyBuildingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Body Building"),
+      ),
+      body: Center(
+        child: Text("Welcome"),
+      ),
+    );
+  }
+}
